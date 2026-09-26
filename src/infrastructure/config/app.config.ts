55928af5e8f86b64ffb9dbbed8dd1daad8abab3e
as3hr <asheressani@gmail.com>
2026-09-26 15:59:06 +0500

    updated app-config

--- src/infrastructure/config/app.config.ts
+++ src/infrastructure/config/app.config.ts
@@ -1,19 +1,11 @@
 interface AppConfig {
-  port: number;
   supabaseUrl: string;
   supabaseKey: string;
-  secret: string;
-  cronSecret: string;
-  nodeEnv: string;
   openRouterApiKey: string;
 }
 
 export const appConfig: AppConfig = {
-  port: parseInt(process.env.PORT || "5000", 10),
   supabaseUrl: process.env.SUPABASE_URL!,
   supabaseKey: process.env.SUPABASE_KEY!,
-  secret: process.env.SECRET!,
-  cronSecret: process.env.CRON_SECRET!,
-  nodeEnv: process.env.NODE_ENV!,
   openRouterApiKey: process.env.OPENROUTER_API_KEY!,
 };
